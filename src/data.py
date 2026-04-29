023876794ce02ea49ddb27ea0f5ce4e2f387accb
leeseungmin <cldkdi1@naver.com>
2026-04-29 23:01:32 +0900

    Refactor: data.py 파일을 src 폴더로 이동

--- src/data.py
+++ src/data.py
@@ -1,23 +1,23 @@
 import pandas as pd
 
-# 자취방 데이터  정제
-df_room = pd.read_excel('data/HOUSE.xlsx')
+# 자취방 데이터 정제
+df_house = pd.read_excel('data/HOUSE.xlsx')
 
 # 실제 엑셀 파일의 컬럼명에 맞게 리스트 수정
 room_cols = ['시군구', '번지', '본번', '부번', '건물명', '전월세구분', '전용면적(㎡)', '계약년월', '보증금(만원)', '월세금(만원)', '층', '건축년도']
-df_room = df_room[room_cols]
+df_house = df_house[room_cols]
 
 # 결측치 제거
-df_room = df_room.dropna()
+df_house = df_house.dropna()
 
 # 주소 분리
-addr_split = df_room['시군구'].str.split(' ', expand=True)
-df_room['시도'] = addr_split[0]
-df_room['구'] = addr_split[1]
-df_room['법정동'] = addr_split[2]
+addr_split = df_house['시군구'].str.split(' ', expand=True)
+df_house['시도'] = addr_split[0]
+df_house['구'] = addr_split[1]
+df_house['법정동'] = addr_split[2]
 
 # 전용면적 9평미만 필터링
-df_room = df_room[df_room['전용면적(㎡)'] <= 30]
+df_house = df_house[df_house['전용면적(㎡)'] <= 30]
 
 # CCTV 데이터 정제
 df_cctv = pd.read_csv('data/CCTV.csv', encoding='cp949')
@@ -30,12 +30,12 @@ df_bus = df_bus[['위도', '경도', '정류장명']]
 df_bus = df_bus.dropna()
 
 # 지하철 데이터 정제
-df_subway = pd.read_csv('data/SUBWAY.csv', encoding='cp949')
-df_subway = df_subway[['선명', '역명', '도로명주소']]
+df_subway = pd.read_excel('data/SUBWAY.xlsx')
+df_subway = df_subway[['역명', '위도', '경도', '호선']]
 df_subway = df_subway.dropna()
 
 # 정제된 데이터를 새로운 파일로 저장
-df_room.to_csv('data/cleaned_HOUSE.csv', index=False, encoding='utf-8-sig')
+df_house.to_csv('data/cleaned_HOUSE.csv', index=False, encoding='utf-8-sig')
 df_cctv.to_csv('data/cleaned_CCTV.csv', index=False, encoding='utf-8-sig')
 df_bus.to_csv('data/cleaned_BUS.csv', index=False, encoding='utf-8-sig')
 df_subway.to_csv('data/cleaned_SUBWAY.csv', index=False, encoding='utf-8-sig')
